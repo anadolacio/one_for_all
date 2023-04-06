231e97d8567b21038e4088b13f5789d56527810d
anadolacio <anadolacio@hotmail.com>
2023-04-06 16:59:19 -0300

    resolução desafio 7

---
+++ challenges/desafio7.sql
@@ -0,0 +1,12 @@
+SELECT 
+    ar.nome_artista AS artista,
+    al.album AS album,
+    COUNT(f.artista_id) AS pessoas_seguidoras
+FROM
+    SpotifyClone.artist AS ar
+INNER JOIN
+    SpotifyClone.albuns AS al ON al.artista_id = ar.artista_id
+INNER JOIN
+    SpotifyClone.followers AS f ON ar.artista_id = f.artista_id
+GROUP BY artista, album
+ORDER BY pessoas_seguidoras DESC, album ASC;
